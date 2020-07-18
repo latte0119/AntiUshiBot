afc247c246da68c72d8dff888bd87767b01cea74
latte0119 <latte.malta@gmail.com>
2020-07-18 17:44:55 +0900

    reply

--- main.go
+++ main.go
@@ -2,18 +2,30 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"net/url"
+	//"time"
 )
 
 func main() {
 	api := GetTwitterAPI()
 
-	t := time.Now()
-	tweet, err := api.PostTweet("うしたぷにきあくん楽しみ〜"+t.String(), nil)
+	/*
+		t := time.Now()
+		tweet, err := api.PostTweet("うしたぷにきあくん楽しみ〜"+t.String(), nil)
+	*/
 
-	if err == nil {
-		fmt.Println(tweet.Text)
-	} else {
-		fmt.Println("uku")
+	v := url.Values{}
+	v.Set("screen_name", "_ei13333")
+	timeline, _ := api.GetUserTimeline(v)
+
+	for _, tw := range timeline {
+		fmt.Println(tw.FullText)
 	}
+
+	dest := timeline[0].IdStr
+
+	v = url.Values{}
+	v.Set("in_reply_to_status_id", dest)
+	v.Set("auto_populate_reply_metadata", "true")
+	api.PostTweet("でも君，う し た ぷ に き あ く ん 笑 じゃん", v)
 }
