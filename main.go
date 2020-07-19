fbaae35a162902c527ef59cd5d49f3c4b78d06ce
latte0119 <latte.malta@gmail.com>
2020-07-19 09:46:02 +0900

    complete bug fix

--- main.go
+++ main.go
@@ -12,14 +12,16 @@ func main() {
 
 	//	api := GetTwitterAPI()
 	db := GetDB()
+	defer db.Close()
 
 	var num int
 	db.QueryRow("select count(*) from tweet").Scan(&num)
-	fmt.Println(num)
-	row := db.QueryRow("select id,text from tweet where id= $1 ", rand.Intn(3))
+	row := db.QueryRow("select id,text from tweet where id= $1 ", rand.Intn(num))
 
 	var id int
 	var text string
 	row.Scan(&id, &text)
+
+	fmt.Println(id, text)
 	///api.PostTweet(text+" KOREHA TESUTO DESU", nil)
 }
