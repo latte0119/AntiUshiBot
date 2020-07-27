2bb3fa3b70f8288414461140a410b55cf1ee41f9
latte0119 <latte.malta@gmail.com>
2020-07-28 03:27:04 +0900

    modify PostDMToMe

--- directMessage.go
+++ directMessage.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"log"
 	"net/http"
 	"os"
 	"strings"
@@ -9,11 +8,14 @@ import (
 	"github.com/dghubble/oauth1"
 )
 
-func PostDMToMe(text string) {
+func PostDMToMe(text string) error {
 	text = EscapeProcessing(text)
 	url := "https://api.twitter.com/1.1/direct_messages/events/new.json"
 	payload := strings.NewReader("{\n\t\"event\":{\n\t\t\t\"type\":\"message_create\",\n\t\t\"message_create\": {\n    \"target\": {\n      \"recipient_id\": \"1284149601826598913\"\n    },\n    \"message_data\": {\n      \"text\": \"" + text + "\"\n    }\n  }\n\t}\n}")
-	req, _ := http.NewRequest("POST", url, payload)
+	req, err := http.NewRequest("POST", url, payload)
+	if err != nil {
+		return err
+	}
 
 	config := oauth1.NewConfig(os.Getenv("CONSUMER_KEY"), os.Getenv("CONSUMER_SECRET"))
 	token := oauth1.NewToken(os.Getenv("ACCESS_TOKEN"), os.Getenv("ACCESS_TOKEN_SECRET"))
@@ -21,9 +23,9 @@ func PostDMToMe(text string) {
 
 	req.Header.Add("content-type", "application/json")
 
-	_, err := httpClient.Do(req)
-	if err != nil {
-		log.Fatal(err)
+	if _, err := httpClient.Do(req); err != nil {
+		return err
 	}
 
+	return nil
 }
